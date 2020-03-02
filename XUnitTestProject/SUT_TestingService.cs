33d175650a3318e6c8e1976857693461510264f2
Dmitry Mudrak <dmitry.mud@mintymint.net>
2020-03-02 21:37:02 +0200

    [tests] added mocked dependencies, no clue what after that

--- XUnitTestProject/SUT_TestingService.cs
+++ XUnitTestProject/SUT_TestingService.cs
@@ -1,4 +1,9 @@
+using Moq;
+using Sharie.Services;
+using Sharie.Services.ExternalService;
+using Sharie.Services.RandomService;
 using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace XUnitTestProject
@@ -6,8 +11,21 @@ namespace XUnitTestProject
     public class SUT_TestingService
     {
         [Fact]
-        public void TestingService_if_random_value_morethan_1000()
+        public void TestingService_if_random_value_morethan_1000_throw_exception()
         {
+            ///act
+            var mockedRandomService = new Mock<IRandomService>();
+            mockedRandomService.Setup(x => x.GenerateRandomValue(2000, 2000)).Returns(2000);
+            var mockedExternalService = new Mock<IExternalSleepyService>();
+            mockedExternalService.Setup(x => x.SleepForATime()).Returns(Task.FromResult(true));
+
+            var mockedTestingService = new Mock<ITestingService>(mockedRandomService, mockedExternalService);
+            mockedTestingService.Setup(x => x.CalculateSomethingRandom(1, 1)).Throws(new Exception());
+
+            //var result = 
+            ////arrange
+
+
 
         }
     }
