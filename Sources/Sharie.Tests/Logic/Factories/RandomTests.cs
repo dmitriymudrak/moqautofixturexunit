34ba32385ff15e5111e4dbff5f7d3d916b805f15
vasiliy.stan <vasiliyStankevich@gmail.com>
2020-03-04 10:03:33 +0200

    work on Unit Tests + refactoring

---
+++ Sources/Sharie.Tests/Logic/Factories/RandomTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Sharie.Logic.Factories;
+using Tests.AAAPattern.xUnit.Attributes;
+using Xunit;
+
+namespace Sharie.Tests.Logic.Factories
+{
+    public class FactoryRandomTests
+    {
+        [Theory]
+        [MoqAutoData]
+        public void CreateTest(int seed, FactoryRandom sut)
+        {
+            //act
+            var expected = sut.Create(seed);
+
+            //assert
+            Assert.NotNull(expected);
+            Assert.IsType<Random>(expected);
+        }
+
+        [Theory]
+        [MoqInlineAutoData(0, 1000)]
+        public void GetSeedTest(int min, int max, FactoryRandom sut)
+        {
+            //act
+            var expected = sut.GenerateSeed();
+
+            //assert
+            Assert.InRange(expected, min, max);
+        }
+    }
+}
