7e187fad9d4cc1d0f3cc4163bb23f4068b35dbcc
vasiliy.stan <vasiliyStankevich@gmail.com>
2020-03-04 09:31:32 +0200

    work on Unit Tests + refactoring

---
+++ Sources/Sharie.Tests/Logic/FactoryTestingModelTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Sharie.Logic;
+using Sharie.Models;
+using Tests.AAAPattern.xUnit.Attributes;
+using Xunit;
+
+namespace Sharie.Tests.Logic
+{
+    public class FactoryTestingModelTests
+    {
+        [Theory]
+        [MoqAutoData]
+        public void EmptyTest(FactoryTestingModel sut)
+        {
+            //act
+            var expected = sut.Empty();
+
+            //assert
+            Assert.NotNull(expected);
+            Assert.IsType<TestingModel>(expected);
+            Assert.Null(expected.OperationType);
+            Assert.Null(expected.RandomValue);
+            Assert.Equal(0, expected.Value);
+        }
+
+        [Theory]
+        [MoqAutoData]
+        public void CreateTest(string operationType, int randomValue, int value, FactoryTestingModel sut)
+        {
+            //act
+            var expected = sut.Create(operationType, randomValue, value);
+
+            //assert
+            Assert.NotNull(expected);
+            Assert.IsType<TestingModel>(expected);
+            Assert.Equal(expected.OperationType, operationType);
+            Assert.Equal(expected.RandomValue, randomValue.ToString());
+            Assert.Equal(expected.Value, value);
+        }
+    }
+}
